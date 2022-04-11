c0a28127dfef5f28788a9e09d6f5ae311001050a
Adrian Moore <adrianrob@hotmail.com>
2022-04-11 13:00:50 -0700

    Updated worker example to show correct use instanced meshes

--- src/app/examples/worker/worker.component.ts
+++ src/app/examples/worker/worker.component.ts
@@ -1,25 +1,29 @@
 import { Component } from "@angular/core";
 
 import { BoxProps, GetByIndex } from "@angular-three/cannon";
+import { NgtTriplet } from "@angular-three/core";
+import { Color, InstancedMesh } from "three";
 
 @Component({
   templateUrl: './worker.component.html'
 })
 export class WorkerComponent {
-  count = 40;
+  count = 100;
+  data = new Array(this.count).fill(0).map((d, index) => ({
+    position: [Math.random() - 0.5, index * 2.5 + 0.5, Math.random() - 0.5] as NgtTriplet,
+    color: new Color().setHex(Math.random() * 0xffffff)
+  }));
 
-  getCubeProps(index: number): BoxProps {
-    return {
-      mass: 1,
-      position: [Math.random() - 0.5, index * 2.5 + 0.5, Math.random() - 0.5]
-    } as BoxProps;
-  }
-
-  getCubePropsThis: GetByIndex<BoxProps> = (index: number) => (
+  getCubeProps: GetByIndex<BoxProps> = (index: number) => (
     {
       mass: 1,
-      position: [Math.random() - 0.5, index * 2.5 + 0.5, Math.random() - 0.5],
-      userData: this.count,
+      position: this.data[index].position,
     }
   );
+
+  ready(inst: InstancedMesh) {
+    this.data.forEach((item, index) => {
+      inst.setColorAt(index, item.color);
+    })
+  }
 }
