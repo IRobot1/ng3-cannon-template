705617f3db38ae532eea944e514c2180c371f1e7
Adrian Moore <adrianrob@hotmail.com>
2022-04-14 15:41:06 -0700

    Changed so all examples in vertical list along side.  Easier to navigate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,51 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-root',
-  template: '<router-outlet></router-outlet>'
+  templateUrl: 'app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent  {
+  examples = [
+    'fps',
+    'conveyor',
+    'conditional',
+    'bounce',
+    'collision_filter',
+    'collisions',
+    'compound',
+    'container',
+    'events',
+    'fixed_rotation',
+    'friction',
+    'hinge',
+    'impulses',
+    'jenga',
+    'mousepick',
+    'performance',
+    'pile',
+    'stacks',
+    'threejs',
+    'trigger',
+    'tween',
+    'worker',
+  ]
+
+  almost = [
+    'constraints',
+    'heightfield',
+    'body_types',
+    'convex',
+    'sleep',
+    'trimesh',
+  ]
+
+  feature = [
+    'ragdoll',
+    'rigid_vehicle',
+    'raycast_vehicle',
+    'cloth',
+    'spring',
+    'tear',
+    'callbacks',
+  ]
 }
--- src/app/home.component.ts
+++ src/app/home.component.ts
@@ -3,50 +3,6 @@ import { Component } from "@angular/core";
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  examples = [
-    'fps',
-    'conveyor',
-    'conditional',
-    'bounce',
-    'collision_filter',
-    'collisions',
-    'compound',
-    'container',
-    'events',
-    'fixed_rotation',
-    'friction',
-    'hinge',
-    'impulses',
-    'jenga',
-    'mousepick',
-    'performance',
-    'pile',
-    'stacks',
-    'threejs',
-    'trigger',
-    'tween',
-    'worker',
-  ]
-
-  almost = [
-    'constraints',
-    'heightfield',
-    'body_types',
-    'convex',
-    'sleep',
-    'trimesh',
-  ]
-
-  feature = [
-    'ragdoll',
-    'rigid_vehicle',
-    'raycast_vehicle',
-    'cloth',
-    'spring',
-    'tear',
-    'callbacks',
-  ]
 }
