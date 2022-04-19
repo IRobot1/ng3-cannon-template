4ca83a494901dc682d39135d88722ed7ac5e9538
Chau Tran <nartc7789@gmail.com>
2022-04-19 16:29:11 -0500

    Clean up and use Camera from NgtStore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,71 +1,164 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgtCannonDebugModule, NgtPhysicsModule } from '@angular-three/cannon';
 
-import { NgtCanvasModule, NgtColorPipeModule, NgtRadianPipeModule, NgtVectorPipeModule } from '@angular-three/core';
-import { NgtStatsModule } from '@angular-three/core/stats';
-import { NgtInstancedMeshModule, NgtMeshModule } from '@angular-three/core/meshes';
+import {
+  NgtCanvasModule,
+  NgtColorPipeModule,
+  NgtRadianPipeModule,
+  NgtVectorPipeModule,
+} from '@angular-three/core';
+import {
+  NgtColorAttributeModule,
+  NgtInstancedBufferAttributeModule,
+} from '@angular-three/core/attributes';
+import {
+  NgtBoxGeometryModule,
+  NgtCylinderGeometryModule,
+  NgtPlaneGeometryModule,
+  NgtPolyhedronGeometryModule,
+  NgtSphereGeometryModule,
+  NgtTorusGeometryModule,
+} from '@angular-three/core/geometries';
 import { NgtGroupModule } from '@angular-three/core/group';
-import { NgtMeshLambertMaterialModule, NgtMeshStandardMaterialModule } from '@angular-three/core/materials';
-import { NgtBoxGeometryModule, NgtCylinderGeometryModule, NgtPlaneGeometryModule, NgtPolyhedronGeometryModule, NgtSphereGeometryModule, NgtTorusGeometryModule } from '@angular-three/core/geometries';
-import { NgtAmbientLightModule, NgtDirectionalLightModule, NgtPointLightModule, NgtSpotLightModule } from '@angular-three/core/lights';
-import { NgtArrowHelperModule, NgtBoxHelperModule } from '@angular-three/core/helpers';
-import { NgtInstancedBufferAttributeModule } from '@angular-three/core/attributes';
-
-import { NgtPhysicsModule } from '@angular-three/cannon';
+import {
+  NgtArrowHelperModule,
+  NgtBoxHelperModule,
+} from '@angular-three/core/helpers';
+import {
+  NgtAmbientLightModule,
+  NgtDirectionalLightModule,
+  NgtPointLightModule,
+  NgtSpotLightModule,
+} from '@angular-three/core/lights';
+import {
+  NgtMeshLambertMaterialModule,
+  NgtMeshStandardMaterialModule,
+} from '@angular-three/core/materials';
+import {
+  NgtInstancedMeshModule,
+  NgtMeshModule,
+} from '@angular-three/core/meshes';
+import { NgtStatsModule } from '@angular-three/core/stats';
 
 import { NgtSobaTextModule } from '@angular-three/soba/abstractions';
 import { NgtSobaOrbitControlsModule } from '@angular-three/soba/controls';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { ConveyorVolumeComponent } from './components/conveyor-volume.component';
+import { FirstPersonControlsComponent } from './components/first-person-controls.component';
+import { FloorComponent } from './components/floor.component';
+import { RagdollModelComponent } from './components/ragdoll-model/ragdoll-model.component';
+import { RaycastVehicleModelComponent } from './components/raycast-vehicle-model/raycast-vehicle-model.component';
+import { RigidBodyModelComponent } from './components/rigidbody-model/rigidbody-model.component';
 import { CubeComponent } from './components/storybook-cube.component';
-import { NgtCannonDebugModule } from '@angular-three/cannon';
 import { TriggerCubeComponent } from './components/trigger-cube.component';
+import {
+  BodyTypesComponent,
+  BodyTypesExample,
+} from './examples/body_types/body_types.component';
+import {
+  BounceComponent,
+  BounceExample,
+} from './examples/bounce/bounce.component';
+import {
+  CallbacksComponent,
+  CallbacksExample,
+} from './examples/callbacks/callbacks.component';
+import { ClothComponent, ClothExample } from './examples/cloth/cloth.component';
+import {
+  CollisionFilterComponent,
+  CollisionFilterExample,
+} from './examples/collision_filter/collision_filter.component';
+import {
+  CollisionsComponent,
+  CollisionsExample,
+} from './examples/collisions/collisions.component';
+import {
+  CompoundComponent,
+  CompoundExample,
+} from './examples/compound/compound.component';
+import { ConditionalComponent } from './examples/conditional/conditional.component';
+import {
+  ConstraintsComponent,
+  ConstraintsExample,
+} from './examples/constraints/constraints.component';
+import {
+  ContainerComponent,
+  ContainerExample,
+} from './examples/container/container.component';
+import {
+  ConvexComponent,
+  ConvexExample,
+} from './examples/convex/convex.component';
+import { ConveyorComponent } from './examples/conveyor/conveyor.component';
+import {
+  EventsComponent,
+  EventsExample,
+} from './examples/events/events.component';
+import {
+  FixedRotationComponent,
+  FixedRotationExample,
+} from './examples/fixed_rotation/fixed_rotation.component';
+import { FPSComponent, FPSExample } from './examples/fps/fps.component';
+import {
+  FrictionComponent,
+  FrictionExample,
+} from './examples/friction/friction.component';
+import {
+  HeightfieldComponent,
+  HeightfieldExample,
+} from './examples/heightfield/heightfield.component';
+import { HingeComponent, HingeExample } from './examples/hinge/hinge.component';
+import {
+  ImpulseComponent,
+  ImpulseExample,
+} from './examples/impulse/impulse.component';
+import { JengaComponent, JengaExample } from './examples/jenga/jenga.component';
+import {
+  MousePickComponent,
+  MousePickExample,
+} from './examples/mousepick/mousepick.component';
+import {
+  PerformanceComponent,
+  PerformanceExample,
+} from './examples/performance/performance.component';
+import { PileComponent, PileExample } from './examples/pile/pile.component';
+import { RagdollComponent } from './examples/ragdoll/ragdoll.component';
+import { RaycastVehicleComponent } from './examples/raycast_vehicle/raycast_vehicle.component';
+import { RigidVehicleComponent } from './examples/rigid_vehicle/rigid_vehicle.component';
+import { SleepComponent, SleepExample } from './examples/sleep/sleep.component';
+import {
+  SpringComponent,
+  SpringExample,
+} from './examples/spring/spring.component';
+import {
+  StacksComponent,
+  StacksExample,
+} from './examples/stacks/stacks.component';
+import { TearComponent, TearExample } from './examples/tear/tear.component';
+import {
+  ThreeJSComponent,
+  ThreeJSExample,
+} from './examples/threejs/threejs.component';
+import {
+  TriggerComponent,
+  TriggerExample,
+} from './examples/trigger/trigger.component';
+import {
+  TrimeshComponent,
+  TrimeshExample,
+} from './examples/trimesh/trimesh.component';
+import { TweenComponent, TweenExample } from './examples/tween/tween.component';
+import { VRComponent, VRExample } from './examples/vr/vr.component';
+import {
+  WorkerComponent,
+  WorkerExample,
+} from './examples/worker/worker.component';
+import { HomeComponent } from './home.component';
 import { XRBatComponent } from './xr-bat/xr-bat.component';
 import { XRInspectComponent } from './xr-inspect/xr-inspect.component';
-import { AppRoutingModule } from './app-routing.module';
-import { VRComponent, VRExample } from './examples/vr/vr.component';
-import { HomeComponent } from './home.component';
-import { ThreeJSComponent, ThreeJSExample } from './examples/threejs/threejs.component';
-import { WorkerComponent, WorkerExample } from './examples/worker/worker.component';
-import { BounceComponent, BounceExample } from './examples/bounce/bounce.component';
-import { CollisionFilterComponent, CollisionFilterExample } from './examples/collision_filter/collision_filter.component';
-import { CollisionsComponent, CollisionsExample } from './examples/collisions/collisions.component';
-import { CompoundComponent, CompoundExample } from './examples/compound/compound.component';
-import { ContainerComponent, ContainerExample } from './examples/container/container.component';
-import { EventsComponent, EventsExample } from './examples/events/events.component';
-import { FixedRotationComponent, FixedRotationExample } from './examples/fixed_rotation/fixed_rotation.component';
-import { FrictionComponent, FrictionExample } from './examples/friction/friction.component';
-import { ImpulseComponent, ImpulseExample } from './examples/impulse/impulse.component';
-import { JengaComponent, JengaExample } from './examples/jenga/jenga.component';
-import { TriggerComponent, TriggerExample } from './examples/trigger/trigger.component';
-import { TrimeshComponent, TrimeshExample } from './examples/trimesh/trimesh.component';
-import { SleepComponent, SleepExample } from './examples/sleep/sleep.component';
-import { TweenComponent, TweenExample } from './examples/tween/tween.component';
-import { ConvexComponent, ConvexExample } from './examples/convex/convex.component';
-import { BodyTypesComponent, BodyTypesExample } from './examples/body_types/body_types.component';
-import { PerformanceComponent, PerformanceExample } from './examples/performance/performance.component';
-import { PileComponent, PileExample } from './examples/pile/pile.component';
-import { StacksComponent, StacksExample } from './examples/stacks/stacks.component';
-import { ClothComponent, ClothExample } from './examples/cloth/cloth.component';
-import { FPSComponent, FPSExample } from './examples/fps/fps.component';
-import { FirstPersonControlsComponent } from './components/first-person-controls.component';
-import { ConveyorComponent } from './examples/conveyor/conveyor.component';
-import { ConveyorVolumeComponent } from './components/conveyor-volume.component';
-import { ConditionalComponent } from './examples/conditional/conditional.component';
-import { MousePickComponent, MousePickExample } from './examples/mousepick/mousepick.component';
-import { CallbacksComponent, CallbacksExample } from './examples/callbacks/callbacks.component';
-import { HeightfieldComponent, HeightfieldExample } from './examples/heightfield/heightfield.component';
-import { HingeComponent, HingeExample } from './examples/hinge/hinge.component';
-import { ConstraintsComponent, ConstraintsExample } from './examples/constraints/constraints.component';
-import { RagdollComponent } from './examples/ragdoll/ragdoll.component';
-import { RagdollModelComponent } from './components/ragdoll-model/ragdoll-model.component';
-import { SpringComponent, SpringExample } from './examples/spring/spring.component';
-import { TearComponent, TearExample } from './examples/tear/tear.component';
-import { RigidVehicleComponent } from './examples/rigid_vehicle/rigid_vehicle.component';
-import { RigidBodyModelComponent } from './components/rigidbody-model/rigidbody-model.component';
-import { RaycastVehicleComponent } from './examples/raycast_vehicle/raycast_vehicle.component';
-import { RaycastVehicleModelComponent } from './components/raycast-vehicle-model/raycast-vehicle-model.component';
-import { FloorComponent } from './components/floor.component';
 
 @NgModule({
   declarations: [
@@ -187,14 +280,14 @@ import { FloorComponent } from './components/floor.component';
     NgtMeshStandardMaterialModule,
 
     NgtPhysicsModule,
-    
+
     NgtCannonDebugModule,
 
-    NgtSobaOrbitControlsModule, 
+    NgtSobaOrbitControlsModule,
     NgtSobaTextModule,
-
+    NgtColorAttributeModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/examples/mousepick/mousepick.component.ts
+++ src/app/examples/mousepick/mousepick.component.ts
@@ -1,11 +1,10 @@
-import { AfterViewInit, Component, Input, OnDestroy } from "@angular/core";
+import { NgtPhysicBody, NgtPhysicConstraint } from '@angular-three/cannon';
 
-import { Camera, Mesh, Raycaster, Vector2, Vector3 } from "three";
+import { NgtStore, NgtTriple } from '@angular-three/core';
+import { DOCUMENT } from '@angular/common';
+import { AfterViewInit, Component, Inject, OnDestroy } from '@angular/core';
 
-import { NgtState, NgtTriple } from "@angular-three/core";
-
-import { NgtPhysicBody, NgtPhysicConstraintReturn } from "@angular-three/cannon";
-import { NgtPhysicConstraint } from "@angular-three/cannon";
+import { Camera, Mesh, Raycaster, Vector2, Vector3 } from 'three';
 
 @Component({
   selector: 'mousepick-example',
@@ -13,8 +12,6 @@ import { NgtPhysicConstraint } from "@angular-three/cannon";
   providers: [NgtPhysicBody, NgtPhysicConstraint],
 })
 export class MousePickExample implements AfterViewInit, OnDestroy {
-  @Input() camera!: Camera;
-
   velocity = [0, 0, 0] as NgtTriple;
   sphereRadius = 0.2;
 
@@ -30,106 +27,102 @@ export class MousePickExample implements AfterViewInit, OnDestroy {
   sphereProps = this.physicBody.useSphere(() => ({
     mass: 0,
     args: [this.sphereRadius],
-    collisionFilterGroup: 0
+    collisionFilterGroup: 0,
   }));
-
-  get options(): Record<string, any> {
-    return {
-      pivotA: [0, 0, 0],
-      pivotB: [0, 0, 0],
-    }
-  }
 
   constructor(
     private physicBody: NgtPhysicBody,
     private physicConstraint: NgtPhysicConstraint,
-  ) { }
-
-
-  cubeMesh!: Mesh;
-  cubeready(mesh: Mesh) {
-    this.cubeMesh = mesh;
-  }
-
+    private store: NgtStore,
+    @Inject(DOCUMENT) private document: Document
+  ) {}
 
   movementPlane!: Mesh;
-  moveready(mesh: Mesh) {
-    this.movementPlane = mesh;
-  }
 
   isDragging = false;
-  constraint!: NgtPhysicConstraintReturn<'PointToPoint'>;
 
   private velocity_subscription?: () => void;
 
   ngAfterViewInit(): void {
-    this.constraint = this.physicConstraint.usePointToPointConstraint(
+    const camera = this.store.get((s) => s.camera);
+
+    const constraint = this.physicConstraint.usePointToPointConstraint(
       this.boxProps.ref,
       this.sphereProps.ref,
       {
         pivotA: [0, 0, 0],
         pivotB: [0, 0, 0],
-      });
-    this.constraint.api.disable();
+      }
+    );
+    constraint.api.disable();
 
     const pointerdown = (event: PointerEvent) => {
       // Cast a ray from where the mouse is pointing and
       // see if we hit something
-      const hitPoint = this.getHitPoint(event.clientX, event.clientY, this.cubeMesh, this.camera)
+      const hitPoint = this.getHitPoint(
+        event.clientX,
+        event.clientY,
+        this.boxProps.ref.value as Mesh,
+        camera
+      );
 
       // if the cube was hit
       if (hitPoint) {
         // Move marker mesh on contact point
-        this.moveClickMarker(hitPoint)
+        this.moveClickMarker(hitPoint);
 
-        this.constraint.api.enable();
+        constraint.api.enable();
 
         // enable constraint between the marker and cube
-        this.isDragging = true
+        this.isDragging = true;
       }
-    }
-    document.body.addEventListener('pointerdown', pointerdown);
+    };
+    this.document.body.addEventListener('pointerdown', pointerdown);
 
     const pointermove = (event: PointerEvent) => {
       if (this.isDragging) {
         // Project the mouse onto the movement plane
-        const hitPoint = this.getHitPoint(event.clientX, event.clientY, this.movementPlane, this.camera)
+        const hitPoint = this.getHitPoint(
+          event.clientX,
+          event.clientY,
+          this.movementPlane,
+          camera
+        );
 
         if (hitPoint) {
           // Move marker mesh on the contact point
-          this.moveClickMarker(hitPoint)
+          this.moveClickMarker(hitPoint);
 
           if (!this.velocity_subscription) {
             // monitor velocity of dragged box until its released
-            this.velocity_subscription = this.boxProps.api.velocity.subscribe(next => {
-              this.velocity = next;
-            });
+            this.velocity_subscription = this.boxProps.api.velocity.subscribe(
+              (next) => {
+                this.velocity = next;
+              }
+            );
           }
-
         }
       }
-    }
-    document.body.addEventListener('pointermove', pointermove);
-
+    };
+    this.document.body.addEventListener('pointermove', pointermove);
 
     const pointerup = (event: PointerEvent) => {
-
       // velocity of other box is now velocity of dragged box
       this.velocity_subscription?.();
       this.velocity_subscription = undefined;
 
-      this.constraint.api.disable();
+      constraint.api.disable();
 
       // disable constraint between marker and box
-      this.isDragging = false
-    }
-    document.body.addEventListener('pointerup', pointerup);
+      this.isDragging = false;
+    };
+    this.document.body.addEventListener('pointerup', pointerup);
 
     this.cleanup = () => {
-      document.body.removeEventListener('pointerdown', pointerdown);
-      document.body.removeEventListener('pointermove', pointermove);
-      document.body.removeEventListener('pointerup', pointerup);
-    }
+      this.document.body.removeEventListener('pointerdown', pointerdown);
+      this.document.body.removeEventListener('pointermove', pointermove);
+      this.document.body.removeEventListener('pointerup', pointerup);
+    };
   }
 
   ngOnDestroy(): void {
@@ -139,23 +132,27 @@ export class MousePickExample implements AfterViewInit, OnDestroy {
   //
   // Returns an hit point if there's a hit with the mesh, otherwise returns undefined
   //
-  private getHitPoint(clientX: number, clientY: number, mesh: Mesh, camera: Camera): Vector3 | undefined {
+  private getHitPoint(
+    clientX: number,
+    clientY: number,
+    mesh: Mesh,
+    camera: Camera
+  ): Vector3 | undefined {
     // Get 3D point form the client x y
-    const mouse = new Vector2()
-    mouse.x = (clientX / window.innerWidth) * 2 - 1
-    mouse.y = -((clientY / window.innerHeight) * 2 - 1)
+    const mouse = new Vector2();
+    mouse.x = (clientX / window.innerWidth) * 2 - 1;
+    mouse.y = -((clientY / window.innerHeight) * 2 - 1);
 
     const raycaster = new Raycaster();
     // Get the picking ray from the point
-    raycaster.setFromCamera(mouse, camera)
+    raycaster.setFromCamera(mouse, camera);
 
     // Find out if there's a hit
-    const hits = raycaster.intersectObject(mesh)
+    const hits = raycaster.intersectObject(mesh);
 
     // Return the closest hit or undefined
-    return hits.length > 0 ? hits[0].point : undefined
+    return hits.length > 0 ? hits[0].point : undefined;
   }
-
 
   // update marker position
   private moveClickMarker(position: Vector3) {
@@ -166,9 +163,4 @@ export class MousePickExample implements AfterViewInit, OnDestroy {
 @Component({
   templateUrl: './mousepick.component.html',
 })
-export class MousePickComponent {
-  camera!: Camera;
-  created(event: NgtState) {
-    this.camera = event.camera;
-  }
-}
+export class MousePickComponent {}
