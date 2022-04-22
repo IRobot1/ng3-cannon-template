8fead7a79a407c68342b2faa0c50cc5b82eb90e1
Adrian Moore <adrianrob@hotmail.com>
2022-04-21 19:51:34 -0700

    Add support for redirecting to specific example from query parameters

--- src/app/home.component.ts
+++ src/app/home.component.ts
@@ -1,8 +1,20 @@
 import { Component } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
+  constructor(
+    private router: ActivatedRoute,
+    private route: Router,
+  ) {
+    this.router.queryParams.subscribe(next => {
+      const example = next['example'];
+      if (example) {
+        this.route.navigate(['/' + example]);
+      }
+    })
+  }
 }
