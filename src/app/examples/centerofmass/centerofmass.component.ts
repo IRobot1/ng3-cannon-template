1b085822078c1838576dfa47a517e537b023131e
Adrian Moore <adrianrob@hotmail.com>
2022-04-24 09:35:51 -0700

    Use constraint to put center of mass outside body

--- src/app/examples/centerofmass/centerofmass.component.ts
+++ src/app/examples/centerofmass/centerofmass.component.ts
@@ -1,26 +1,54 @@
 import { Component } from "@angular/core";
 
-import { NgtPhysicBody } from "@angular-three/cannon";
+import { AfterViewInit } from "@angular/core";
+
+import { NgtPhysicBody, NgtPhysicConstraint } from "@angular-three/cannon";
 
 @Component({
   selector: 'centerofmass-example',
   templateUrl: 'centerofmass-example.component.html',
-  providers: [NgtPhysicBody],
+  providers: [NgtPhysicBody, NgtPhysicConstraint],
 })
-export class CenterOfMassExample {
+export class CenterOfMassExample implements AfterViewInit {
 
-  cylinder = this.physicBody.useCylinder(() => ({
+  cone = this.physicBody.useCylinder(() => ({
     mass: 1,
-    args: [0, 1, 1, 20],
-    position: [0, 2, 0],
-    rotation: [3.14, 0, 0],
+    args: [1, 0, 1, 20],
+    position: [0, 5, 0],
+    rotation: [0, 0, 0],
+    angularDamping: 0.5,
+    linearDamping: 0.5,
   }))
 
-  constructor(private physicBody: NgtPhysicBody) { }
+  particlesize = 0.1
+  particle = this.physicBody.useSphere(() => ({
+    mass: 2,
+    args: [this.particlesize],
+    position: [0, 3, 0],
+    angularDamping: 0.5,
+    linearDamping: 0.5,
+    collisionResponse: false,
+  }))
+
+  constructor(
+    private physicBody: NgtPhysicBody,
+    private physicConstraint: NgtPhysicConstraint,
+  ) { }
+
+  ngAfterViewInit(): void {
+    this.physicConstraint.useConeTwistConstraint(
+      this.particle.ref, this.cone.ref, {
+        pivotA: [0, 0, 0],
+        pivotB: [0, -2, 0],
+        
+    });
+    this.cone.api.rotation.set(1, 0, 0);
+  }
 }
 
 @Component({
   templateUrl: './centerofmass.component.html',
 })
 export class CenterOfMassComponent {
+  debug = true;
 }
